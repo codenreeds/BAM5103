e5c1263070151dd77babcec4736518e7af199b8f
Daehun Song <codenreeds@skku.edu>
2026-08-29 15:59:54 +0900

    unzip function addedd

--- download.py
+++ download.py
@@ -2,3 +2,11 @@ import requests
 
 r = requests.get("https://github.com/pimatskku/sturdy-memory/raw/refs/heads/main/dataset.zip")
 open("dataset.zip", "wb").write(r.content)
+
+
+import zipfile
+
+with zipfile.ZipFile("dataset.zip", "r") as zip_ref:
+    zip_ref.extractall("data")
+
+print("Finished")
